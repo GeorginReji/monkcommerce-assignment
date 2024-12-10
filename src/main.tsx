cca2ada907f2b1094a0e145ae84d02586a842e43
GeorginReji <georginreji@gmail.com>
2024-12-10 21:18:29 +0530

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,12 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AddProducts } from './App.tsx';
 import { CssBaseline, CssVarsProvider } from '@mui/joy';
 import { globalTheme } from './Styles/joy_themes.ts';
 
 createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<CssVarsProvider theme={globalTheme} modeStorageKey="user_theme_mode">
-			<CssBaseline />
+	<CssVarsProvider theme={globalTheme} modeStorageKey="user_theme_mode">
+		<CssBaseline />
 
-			<AddProducts />
-		</CssVarsProvider>
-	</StrictMode>
+		<AddProducts />
+	</CssVarsProvider>
 );
